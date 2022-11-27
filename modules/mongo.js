b7b5bf96524e25beff5988383eebe88d84fb6aa4
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2022-11-27 18:16:53 +0300

    mongoose options set

--- modules/mongo.js
+++ modules/mongo.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+
 async function client() {
-  return await mongoose.connect(
-    "mongodb://127.0.0.1/orgo-mall",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  );
+  return await mongoose.connect("mongodb://127.0.0.1/orgo-mall", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 }
 
 module.exports = { client };
