2cd92201c076bb03af6bf6412ce2c21a208b6dd4
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2023-03-26 14:06:48 +0300

    db cluster connection 26.03

--- modules/mongo.js
+++ modules/mongo.js
@@ -5,9 +5,12 @@ mongoose.set("useFindAndModify", false);
 mongoose.set("useCreateIndex", true);
 
 async function client() {
-  return await mongoose.connect("mongodb://127.0.0.1/orgo-mall", {
+  // return await mongoose.connect("localhost:127.0.0.0.1:27017/orgo-mall", {
+  return await mongoose.connect("mongodb+srv://offism:sma01022002@cluster0.iwyiykd.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    // useFindAndModify: false,
+    // useCreateIndex: true
   });
 }
 
